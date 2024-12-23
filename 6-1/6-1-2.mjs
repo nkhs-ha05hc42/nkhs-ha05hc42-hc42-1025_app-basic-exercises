664721641c8e1cbb382b5d6283a04a746198f7d9
nakayama1206 <suito1206@gmail.com>
2024-12-23 11:05:20 +0900

    6-1-2

---
+++ 6-1/6-1-2.mjs
@@ -0,0 +1,17 @@
+const testObject = {
+    num1: 10,
+    num2: 2.5,
+    str1: "test",
+    str2: "B",
+    bool: true,
+    test1: null,
+    test2: undefined,
+    nestObject: {
+            num1: 123,
+            str1: "C"
+        }
+    }
+    console.log(testObject.nun1)
+    console.log(testObject.nun2)
+    console.log(testObject.str2)
+    console.log(testObject.bool)
