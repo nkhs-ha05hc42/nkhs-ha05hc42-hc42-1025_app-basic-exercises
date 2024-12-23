18c2a24e2b5d7fd0e804eda2b9891004a0f481a2
nakayama1206 <suito1206@gmail.com>
2024-12-23 10:41:15 +0900

    komittosiro

---
+++ 5-1/5-2.js
@@ -0,0 +1,11 @@
+// コメント文なので表示、処理されない
+/* これは複数行のコメント文。
+コメント文と同様動作 */
+console.log(400)
+console.log('920')
+console.log(0x000a)
+console.log(false)
+console.log(true)
+console.log([1234, 56])
+console.log(['a', 'b'])
+console.log([false, true])
---
+++ 6-1/6-1-1.mjs
@@ -0,0 +1,13 @@
+const testObject = {
+    num1: 10,
+    num2: 2.5,
+    str1: "test",
+    str2: "B",
+    bool: true,
+    test1: null,
+    test2: undefined,
+    nestObject: {
+    num1: 123,
+    str1: "C"
+    }
+    }
